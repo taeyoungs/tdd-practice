3636a079f1fdd3a958a1881fcf5aa607a6b2a142
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-11-27 11:56:46 +0900

    add: Dollar 클래스에 amount 필드 추가
    multiplication.test.js
    - Dollar => const 키워드로 변경
    - toBe 메서드의 인자로 five.amount를 잘못 전달하고 있던 것을
      10으로 수정
    
    Dollar.js
    - 테스트 코드의 4번째 컴파일 에러를 해결하기 위해 Dollar
      클래스에 amount 멤버 변수 추가

--- chapter/1/__tests__/multiplication.test.js
+++ chapter/1/__tests__/multiplication.test.js
@@ -1,5 +1,8 @@
+import Dollar from '../js/Dollar.js';
+
 test('어떤 금액(주가)을 어떤 수(주식의 수)에 곱한 금액을 결과로 얻을 수 있어야 한다.', () => {
-  Dollar five = new Dollar(5);
+  const five = new Dollar(5);
   five.times(2);
-  expect(five.amount).toBe(five.amount);
+
+  expect(five.amount).toBe(10);
 });
--- chapter/1/js/Dollar.js
+++ chapter/1/js/Dollar.js
@@ -1,5 +1,9 @@
 class Dollar {
+  amount;
+
   constructor(amount) {}
 
   times(multiplier) {}
 }
+
+export default Dollar;
