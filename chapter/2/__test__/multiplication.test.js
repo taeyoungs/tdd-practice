b03f9c7b683ec069f24170d8554ac6a824569818
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-11-28 12:25:01 +0900

    add: Dollar 부작용에 대한 실패 테스트 케이스 작성
    multiplication.test.js
    - Dollar 클래스는 현재 부작용을 가지고 있다. times 메서드를
      호출할 경우 멤버 변수의 값을 변경하기 때문에 생성자 함수에서
      전달받은 초기화 값을 유지하지 못한다.
    - 이러한 부작용이 없다면 통과할 수 있는 하지만 현재는 실패하는
      테스트 케이스를 작성한다.

--- chapter/2/__test__/multiplication.test.js
+++ chapter/2/__test__/multiplication.test.js
@@ -2,7 +2,9 @@ import Dollar from '../js/Dollar.js';
 
 test('어떤 금액(주가)을 어떤 수(주식의 수)에 곱한 금액을 결과로 얻을 수 있어야 한다.', () => {
   const five = new Dollar(5);
-  five.times(2);
 
-  expect(five.amount).toBe(10);
+  let product = five.times(2);
+  expect(product.amount).toBe(10);
+  product = five.times(3);
+  expect(product.amount).toBe(15);
 });
