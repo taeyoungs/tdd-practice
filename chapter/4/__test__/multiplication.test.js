f14aeda8dd7e2ef3741b889f8d3578034d1b974c
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-11-30 13:33:19 +0900

    mod: multiplication에서 product 임시 변수 삭제
    multiplication.test.js
    - product 임시 변수가 더 이상 쓸모가 없으므로 삭제

--- chapter/4/__test__/multiplication.test.js
+++ chapter/4/__test__/multiplication.test.js
@@ -3,8 +3,6 @@ import Dollar from '../js/Dollar.js';
 test('어떤 금액(주가)을 어떤 수(주식의 수)에 곱한 금액을 결과로 얻을 수 있어야 한다.', () => {
   const five = new Dollar(5);
 
-  let product = five.times(2);
-  expect(new Dollar(10).equals(product)).toBe(true);
-  product = five.times(3);
-  expect(new Dollar(15).equals(product)).toBe(true);
+  expect(new Dollar(10).equals(five.times(2))).toBe(true);
+  expect(new Dollar(15).equals(five.times(3))).toBe(true);
 });
