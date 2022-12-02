d42e8ffff81ed854d5da05321651a4d7d2e4be35
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-12-02 12:09:17 +0900

    mod: 임시로 Franc에 대한 동치성 테스트도 추가
    equality.test.js
    - 임시로 Franc 클래스에 대한 동치성 테스트도 추가했다.
      하지만 '복사 + 붙여넣기'라는 죄악을 사용해서 대처했기 때문에
      중복이 발생했다. 따라서, 추후에 이 중복을 제거하는 과정을
      진행해줘야 한다.

--- chapter/6/__test__/equality.test.js
+++ chapter/6/__test__/equality.test.js
@@ -1,6 +1,9 @@
 import Dollar from '../js/Dollar';
+import Franc from '../js/Franc';
 
-test('생성자 함수 인자에 동일한 값을 전달한 Dollar 인스턴스는 같아야 한다.', () => {
+test('생성자 함수 인자에 동일한 값을 전달한 인스턴스는 같아야 한다.', () => {
   expect(new Dollar(5).equals(new Dollar(5))).toBe(true);
   expect(new Dollar(5).equals(new Dollar(6))).toBe(false);
+  expect(new Franc(5).equals(new Franc(5))).toBe(true);
+  expect(new Franc(5).equals(new Franc(6))).toBe(false);
 });
