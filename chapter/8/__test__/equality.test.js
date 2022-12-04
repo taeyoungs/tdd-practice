ae852b23e79f0f4ac78e0f1453f56192427a5b4e
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-12-04 13:58:19 +0900

    mod: 팩토리 메서드를 나머지 테스트에도 적용

--- chapter/8/__test__/equality.test.js
+++ chapter/8/__test__/equality.test.js
@@ -1,10 +1,9 @@
-import Dollar from '../js/Dollar';
-import Franc from '../js/Franc';
+import Money from '../js/Money';
 
 test('생성자 함수 인자에 동일한 값을 전달한 인스턴스는 같아야 한다. 단, 클래스도 동일해야 한다', () => {
-  expect(new Dollar(5).equals(new Dollar(5))).toBe(true);
-  expect(new Dollar(5).equals(new Dollar(6))).toBe(false);
-  expect(new Franc(5).equals(new Franc(5))).toBe(true);
-  expect(new Franc(5).equals(new Franc(6))).toBe(false);
-  expect(new Dollar(5).equals(new Franc(6))).toBe(false);
+  expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true);
+  expect(Money.dollar(5).equals(Money.dollar(6))).toBe(false);
+  expect(Money.franc(5).equals(Money.franc(5))).toBe(true);
+  expect(Money.franc(5).equals(Money.franc(6))).toBe(false);
+  expect(Money.dollar(5).equals(Money.franc(6))).toBe(false);
 });
--- chapter/8/__test__/francMultiplication.test.js
+++ chapter/8/__test__/francMultiplication.test.js
@@ -1,8 +1,8 @@
-import Franc from '../js/Franc';
+import Money from '../js/Money';
 
 test('Franc 곱하기 연산 검증', () => {
-  const five = new Franc(5);
+  const five = Money.franc(5);
 
-  expect(new Franc(10).equals(five.times(2))).toBe(true);
-  expect(new Franc(15).equals(five.times(3))).toBe(true);
+  expect(Money.franc(10).equals(five.times(2))).toBe(true);
+  expect(Money.franc(15).equals(five.times(3))).toBe(true);
 });
