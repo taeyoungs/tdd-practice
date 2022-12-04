cc49d94af66ed679a6eaedf2c7e00475d1583389
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-12-04 13:06:31 +0900

    mod: 팩토리 메서드 패턴을 이용한 곱하기 연산 검증
    multiplication.test.js
    - 하위 클래스에 대한 직접적인 참조를 줄이기 위하여 팩토리
      메서드 패턴을 이용한다. 따라서, 팩토리 메서드 패턴을 도입을 위해
      테스트 코드를 수정

--- chapter/8/__test__/multiplication.test.js
+++ chapter/8/__test__/multiplication.test.js
@@ -1,7 +1,8 @@
 import Dollar from '../js/Dollar.js';
+import Money from '../js/Money.js';
 
 test('Dollar 곱하기 연산 검증', () => {
-  const five = new Dollar(5);
+  const five = Money.dollar(5);
 
   expect(new Dollar(10).equals(five.times(2))).toBe(true);
   expect(new Dollar(15).equals(five.times(3))).toBe(true);
