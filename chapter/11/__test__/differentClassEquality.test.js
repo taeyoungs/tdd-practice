eb0b6aab0a3f49f6cf98136c19fe4128b6b76a85
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-12-10 15:31:43 +0900

    mod: 동치성 테스트 업데이트
    equality.test.js
    - differenctClassEquality.test.js를 제거해도 equality.test.js에서
      동치성 테스트에 대한 테스트가 충분하다는 것을 알 수 있다.
    - 하위 클래스를 비교하는 로직에서 currency, 즉 통화 개념을
      비교하는 로직으로 변경됐으니 세 번째, 네 번째 테스트는
      첫 번째 그리고 두 번째 테스트와 중복된다. 따라서, 중복되는
      테스트는 제거해준다.
    
    differentClassEquality.test.js
    - 하위 클래스에 대한 직접적인 참조를 제거하기 위해 테스트를 삭제
    - equality.test.js로 동치성에 대한 테스트가 보장된다 라는 것을
      알았기 때문에 이를 제거해도 문제가 없다.

--- chapter/11/__test__/differentClassEquality.test.js
+++
@@ -1,5 +0,0 @@
-import { Money, Franc, CURRENCY } from '../js/internal';
-
-test("Franc(10, 'CHF')와 Money(10, 'USD')는 같아야 한다.", () => {
-  expect(new Money(10, CURRENCY.FRANC).equals(new Franc(10, CURRENCY.FRANC))).toBe(true);
-});
--- chapter/11/__test__/equality.test.js
+++ chapter/11/__test__/equality.test.js
@@ -1,9 +1,7 @@
 import { Money } from '../js/internal';
 
-test('생성자 함수 인자에 동일한 값을 전달한 인스턴스는 같아야 한다. 단, 클래스도 동일해야 한다', () => {
+test('동일한 화폐를 사용하는 Money는 금액이 동일할 경우 같아야 한다.', () => {
   expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true);
   expect(Money.dollar(5).equals(Money.dollar(6))).toBe(false);
-  expect(Money.franc(5).equals(Money.franc(5))).toBe(true);
-  expect(Money.franc(5).equals(Money.franc(6))).toBe(false);
   expect(Money.dollar(5).equals(Money.franc(6))).toBe(false);
 });
