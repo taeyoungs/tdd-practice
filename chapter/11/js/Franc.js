a57dc32101b1d60e03b8f9c0dc3c9e8cbeb22323
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-12-10 15:42:06 +0900

    mod: 더 이상 필요없는 Dollar, Franc 클래스 삭제

--- chapter/11/js/Franc.js
+++
@@ -1,7 +0,0 @@
-import { Money } from './internal';
-
-export class Franc extends Money {
-  constructor(amount, currency) {
-    super(amount, currency);
-  }
-}
--- chapter/11/js/internal.js
+++ chapter/11/js/internal.js
@@ -1,4 +1,2 @@
 export * from './Money';
-export * from './Dollar';
-export * from './Franc';
 export * from './constants';
