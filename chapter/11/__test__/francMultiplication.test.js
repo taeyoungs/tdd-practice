09ad883f3f142e7e388893451938c6ac95f654ae
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-12-10 15:46:10 +0900

    mod: 구조가 변경되면서 불필요해진 테스트 삭제

--- chapter/11/__test__/francMultiplication.test.js
+++
@@ -1,8 +0,0 @@
-import { Money } from '../js/internal';
-
-test('Franc 곱하기 연산 검증', () => {
-  const five = Money.franc(5);
-
-  expect(Money.franc(10).equals(five.times(2))).toBe(true);
-  expect(Money.franc(15).equals(five.times(3))).toBe(true);
-});
