bb4994a1e3dd7c4a3cad027450cb05ceffa1e3dc
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-12-10 15:47:39 +0900

    init: Chapter 12 initialize

---
+++ chapter/12/js/Money.js
@@ -0,0 +1,37 @@
+import { Dollar, Franc, CURRENCY } from './internal';
+
+export class Money {
+  #amount;
+  #currency;
+
+  constructor(amount, currency) {
+    this.#amount = amount;
+    this.#currency = currency;
+  }
+
+  get amount() {
+    return this.#amount;
+  }
+
+  get currency() {
+    return this.#currency;
+  }
+
+  equals(instance) {
+    const isSameCurrency = this.currency === instance.currency;
+
+    return this.amount === instance.amount && isSameCurrency;
+  }
+
+  times(multiplier) {
+    return new Money(this.amount * multiplier, this.currency);
+  }
+
+  static dollar(amount) {
+    return new Money(amount, CURRENCY.DOLLAR);
+  }
+
+  static franc(amount) {
+    return new Money(amount, CURRENCY.FRANC);
+  }
+}
---
+++ chapter/12/js/internal.js
@@ -0,0 +1,2 @@
+export * from './Money';
+export * from './constants';
