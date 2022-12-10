4c2dff39fdfd7440cde17437f3b0b6040c2fffea
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-12-10 18:25:09 +0900

    add: Bank 클래스에 reduce 메서드 가짜 구현
    Bank.ts
    - 리팩토링 준비를 마치기 위해 테스트를 우선 빠르게 통과시켜야
      한다. 따라서, 가짜 구현을 통해 reduce 메서드를 구현하여
      테스트를 통과시킨다.

--- chapter/12/js/Bank.ts
+++ chapter/12/js/Bank.ts
@@ -1,3 +1,10 @@
-class Bank {}
+import Expression from './Expression';
+import { Money } from './Money';
+
+class Bank {
+  reduce(source: Expression, to: string): Money {
+    return Money.dollar(10);
+  }
+}
 
 export default Bank;
