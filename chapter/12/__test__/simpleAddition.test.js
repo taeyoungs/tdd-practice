4ba1d604333091ce3d7ac4da2d0c9a6dc9fe28c0
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-12-10 17:30:44 +0900

    add: 테스트 코드에 Bank와 Expression 반영
    simpleAddition.test.js
    - 두 금액의 합을 Expression으로 표현하고 Bank에 reduce에 대한
      책임을 위임했으므로 이를 반영한 테스트 코드로 업데이트한다.

--- chapter/12/__test__/simpleAddition.test.js
+++ chapter/12/__test__/simpleAddition.test.js
@@ -1,6 +1,13 @@
-import { Money } from '../js/internal';
+import { Money, CURRENCY } from '../js/internal';
 
 test('같은 통화를 가진 두 금액 더하기', () => {
-  const sum = Money.dollar(5).plus(Money.dollar(5));
-  expect(Money.dollar(10).equals(sum)).toBe(true);
+  const five = Money.dollar(5);
+
+  const sum = five.plus(five);
+
+  const bank = new Bank();
+
+  const reduced = bank.reduce(sum, CURRENCY.DOLLAR);
+
+  expect(Money.dollar(10).equals(reduced)).toBe(true);
 });
