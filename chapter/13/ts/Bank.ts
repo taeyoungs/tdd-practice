2a230e54cb9d59d6171d3ee4ab522a15aab3ce3d
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-12-12 16:50:41 +0900

    mod: Bank 클래스의 reduce 메서드 1차 수정

--- chapter/13/ts/Bank.ts
+++ chapter/13/ts/Bank.ts
@@ -1,9 +1,15 @@
 import Expression from './Expression';
 import Money from './Money';
+import Sum from './Sum';
+import type { CurrencyTypes } from './constants';
 
 class Bank {
-  reduce(source: Expression, to: string): Money {
-    return Money.dollar(10);
+  reduce(source: Expression, to: CurrencyTypes): Money {
+    const sum = source as Sum;
+
+    const amount = sum.augend.amount + sum.addend.amount;
+
+    return new Money(amount, to);
   }
 }
 
