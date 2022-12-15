ee25113d95a6a4f40243c97ec30b041c8a3e688b
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-12-15 16:12:56 +0900

    add: LinkedList에 사용할 Node 인스턴스 테스트 추가
    node.test.ts
    - Node 인스턴스가 value와 next 속성을 잘 가지고 있는지 검증하는
      테스트 케이스 추가

---
+++ chapter/14/__test__/LinkedList/node.test.ts
@@ -0,0 +1,18 @@
+import Node from '../../ts/LinkedList/Node';
+
+describe('Node 인스턴스 검증', () => {
+  test('Node 인스턴스는 value 값을 저장하고 있어야 한다.', () => {
+    const node = new Node(5);
+
+    expect(node.value).toBe(5);
+  });
+
+  test('Node 인스턴스는 next에 자신의 다음 Node를 저장할 수 있어야 한다.', () => {
+    const node1 = new Node(5);
+    const node2 = new Node(10);
+
+    node1.next = node2;
+
+    expect(node1.next.value).toBe(10);
+  });
+});
