596c00fb989eb7cb8793cd723237aaccd1fe49c6
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-12-15 14:38:58 +0900

    add: Bank에 환율 처리를 위한 rate 메서드 구현

--- chapter/14/ts/Bank.ts
+++ chapter/14/ts/Bank.ts
@@ -1,10 +1,16 @@
 import Expression from './Expression';
 import Money from './Money';
+
+import { CURRENCY } from './constants';
 import type { CurrencyTypes } from './constants';
 
 class Bank {
   reduce(source: Expression, to: CurrencyTypes): Money {
     return source.reduce(this, to);
+  }
+
+  rate(from: CurrencyTypes, to: CurrencyTypes) {
+    return from === CURRENCY.FRANC && to === CURRENCY.DOLLAR ? 2 : 1;
   }
 }
 
