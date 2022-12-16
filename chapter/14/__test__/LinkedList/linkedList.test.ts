8fe6b93d19a6698c89ed53b1438bd08791e4f5c1
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-12-16 18:07:19 +0900

    test(LinkedList): LinkedList get 테스트 케이스 추가
    linkedList.test.ts
    - 특정 Key에 해당하는 value를 얻을 수 있는 get 메서드에 대한
      테스트 케이스 추가
    - 특정 Key와 일치하는 Node가 없다면 null을 반환해야 한다.

--- chapter/14/__test__/LinkedList/linkedList.test.ts
+++ chapter/14/__test__/LinkedList/linkedList.test.ts
@@ -22,9 +22,10 @@ describe('LinkedList 인스턴스 검증', () => {
 
   test('LinkedList 비어 있지 않을 때의 add 연산', () => {
     const pair = new Pair(CURRENCY.FRANC, CURRENCY.DOLLAR);
-    const linkedList = new LinkedList();
     const node1 = new Node(pair, 2);
     const node2 = new Node(pair, 4);
+
+    const linkedList = new LinkedList();
 
     linkedList.add(node1);
     linkedList.add(node2);
@@ -34,5 +35,21 @@ describe('LinkedList 인스턴스 검증', () => {
     expect(node1.equals(linkedList.first)).toBe(true);
     expect(node2.equals(linkedList.last)).toBe(true);
     expect(linkedList.length).toBe(2);
+  });
+
+  test('LinkedList는 특정 키를 가진 Node의 value를 얻을 수 있어야 한다.', () => {
+    const pair1 = new Pair(CURRENCY.FRANC, CURRENCY.DOLLAR);
+    const pair2 = new Pair(CURRENCY.DOLLAR, CURRENCY.FRANC);
+    const node = new Node(pair1, 2);
+
+    const linkedList = new LinkedList();
+
+    linkedList.add(node);
+
+    const value1 = linkedList.get(pair1);
+    const value2 = linkedList.get(pair2);
+
+    expect(value1).toBe(2);
+    expect(value2).toBeNull();
   });
 });
