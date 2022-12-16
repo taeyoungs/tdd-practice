74f794dc2f72eef7b0d66af272fde820585d12c5
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-12-16 17:50:44 +0900

    test(LinkedList): LinkedList add 테스트 케이스 수정
    linkedList.test.ts
    - 변경된 Node 인터페이스에 맞게 테스트 케이스 수정 (제네릭 제거)
    - Node 인스턴스 생성 구문 업데이트

--- chapter/14/__test__/LinkedList/linkedList.test.ts
+++ chapter/14/__test__/LinkedList/linkedList.test.ts
@@ -1,25 +1,30 @@
-import Node from '../../ts/LinkedList/Node';
 import LinkedList from '../../ts/LinkedList';
+import Node from '../../ts/LinkedList/Node';
+import Pair from '../../ts/Pair';
+import { CURRENCY } from '../../ts/constants';
 
 describe('LinkedList 인스턴스 검증', () => {
   test('LinkedList가 비어 있을 때의 add 연산', () => {
-    const linkedList = new LinkedList<number>();
-    const node1 = new Node(5);
+    const pair = new Pair(CURRENCY.FRANC, CURRENCY.DOLLAR);
+    const node = new Node(pair, 2);
 
-    expect(linkedList.contains(node1)).toBe(false);
+    const linkedList = new LinkedList();
 
-    linkedList.add(node1);
+    expect(linkedList.contains(node)).toBe(false);
 
-    expect(linkedList.contains(node1)).toBe(true);
-    expect(node1.equals(linkedList.first)).toBe(true);
-    expect(node1.equals(linkedList.last)).toBe(true);
+    linkedList.add(node);
+
+    expect(linkedList.contains(node)).toBe(true);
+    expect(node.equals(linkedList.first)).toBe(true);
+    expect(node.equals(linkedList.last)).toBe(true);
     expect(linkedList.length).toBe(1);
   });
 
   test('LinkedList 비어 있지 않을 때의 add 연산', () => {
-    const linkedList = new LinkedList<number>();
-    const node1 = new Node(5);
-    const node2 = new Node(10);
+    const pair = new Pair(CURRENCY.FRANC, CURRENCY.DOLLAR);
+    const linkedList = new LinkedList();
+    const node1 = new Node(pair, 2);
+    const node2 = new Node(pair, 4);
 
     linkedList.add(node1);
     linkedList.add(node2);
