7982e33f93f3b2c54cc3ed58f3848e0fa40b8f84
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-12-16 21:38:53 +0900

    test: 같은 통화로의 환율 반환 테스트 케이스 추가

---
+++ chapter/14/__test__/identityRate.test.ts
@@ -0,0 +1,6 @@
+import Bank from '../ts/Bank';
+import { CURRENCY } from '../ts/constants';
+
+test('USD에서 USD로의 환율을 요청하면 그 값이 1이 되어야 한다', () => {
+  expect(new Bank().rate(CURRENCY.DOLLAR, CURRENCY.DOLLAR)).toBe(1);
+});
