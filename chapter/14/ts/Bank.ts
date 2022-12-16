68a7a64b55b40f59777a5964aa5affa53b7c253d
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-12-16 21:32:38 +0900

    mod(Bank): rate, addRate 메서드 수정
    Bank.ts
    - 환율을 저장할 hashTable private 멤버 변수를 추가하고 이를
      이용하도록 rate와 addRate 메서드를 수정

--- chapter/14/ts/Bank.ts
+++ chapter/14/ts/Bank.ts
@@ -1,16 +1,32 @@
 import Expression from './Expression';
 import Money from './Money';
+import HashTable from './HashTable';
+import Pair from './Pair';
 
 import { CURRENCY } from './constants';
 import type { CurrencyTypes } from './constants';
 
 class Bank {
+  #hashTable: HashTable;
+
+  constructor() {
+    this.#hashTable = new HashTable();
+  }
+
   reduce(source: Expression, to: CurrencyTypes): Money {
     return source.reduce(this, to);
   }
 
   rate(from: CurrencyTypes, to: CurrencyTypes) {
-    return from === CURRENCY.FRANC && to === CURRENCY.DOLLAR ? 2 : 1;
+    const pair = new Pair(from, to);
+
+    return this.#hashTable.get(pair);
+  }
+
+  addRate(from: CurrencyTypes, to: CurrencyTypes, rate: number) {
+    const pair = new Pair(from, to);
+
+    this.#hashTable.put(pair, rate);
   }
 }
 
