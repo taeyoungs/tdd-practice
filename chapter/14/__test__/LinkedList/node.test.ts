42dd36cdea728f2b8b35eabe40605d21244d8ef7
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-12-16 15:05:20 +0900

    test: Node equals 메서드에 대한 테스트 케이스 추가

--- chapter/14/__test__/LinkedList/node.test.ts
+++ chapter/14/__test__/LinkedList/node.test.ts
@@ -7,6 +7,13 @@ describe('Node 인스턴스 검증', () => {
     expect(node.value).toBe(5);
   });
 
+  test('동일한 value를 가진 Node 인스턴스는 같아야 한다.', () => {
+    const node1 = new Node(5);
+    const node2 = new Node(5);
+
+    expect(node1.equals(node2)).toBe(true);
+  });
+
   test('Node 인스턴스는 next에 자신의 다음 Node를 저장할 수 있어야 한다.', () => {
     const node1 = new Node(5);
     const node2 = new Node(10);
