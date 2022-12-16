53d35c2eb9c00f8782d1a1c8ae360a1859106eaa
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-12-16 17:10:34 +0900

    feat(Pair): equals, hashCode 메서드 구현

--- chapter/14/ts/Pair.ts
+++ chapter/14/ts/Pair.ts
@@ -1,10 +1,28 @@
-class Pair {
-  #from: string;
-  #to: string;
+import { CurrencyTypes } from './constants';
 
-  constructor(from: string, to: string) {
+class Pair {
+  #from: CurrencyTypes;
+  #to: CurrencyTypes;
+
+  constructor(from: CurrencyTypes, to: CurrencyTypes) {
     this.#from = from;
     this.#to = to;
+  }
+
+  get from() {
+    return this.#from;
+  }
+
+  get to() {
+    return this.#to;
+  }
+
+  equals(newPair: Pair) {
+    return this.from === newPair.from && this.to === newPair.to;
+  }
+
+  hashCode() {
+    return 0;
   }
 }
 
