31f8036c5ff306d5ddb9e46cc33ebd38fb2501f1
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-01-02 13:45:19 +0900

    mod(Sum): 피가산수, 가산수를 Expression으로 변경
    15/ts/Sum.ts
    - 서로 다른 두 통화를 더할 수 있도록 기존에 단순히 amount 값을
      더하는 연산에서 환율 정보를 반영하여 값을 축약하고 축약된 값을
      amount로 하는 Money 인스턴스를 반환하도록 변경
    - 피가산수와 가산수의 타입을 Expression으로 변경

--- chapter/15/ts/Sum.ts
+++ chapter/15/ts/Sum.ts
@@ -5,16 +5,16 @@ import Bank from './Bank';
 import type { CurrencyTypes } from './constants';
 
 class Sum implements Expression {
-  augend: Money;
-  addend: Money;
+  augend: Expression;
+  addend: Expression;
 
-  constructor(augend: Money, addend: Money) {
+  constructor(augend: Expression, addend: Expression) {
     this.augend = augend;
     this.addend = addend;
   }
 
-  reduce(_bank: Bank, to: CurrencyTypes): Money {
-    const amount = this.augend.amount + this.addend.amount;
+  reduce(bank: Bank, to: CurrencyTypes): Money {
+    const amount = this.augend.reduce(bank, to).amount + this.addend.reduce(bank, to).amount;
 
     return new Money(amount, to);
   }
